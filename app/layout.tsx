bdd8d2fac04e5f0990f97cf54cfb098eeea9ceef
thomas0124 <qingshuitouzhen@gmail.com>
2025-04-23 15:19:15 +0900

    OGPの設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,20 @@ export default function RootLayout({
   return (
     <html lang="en"　suppressHydrationWarning　>
       <body className={inter.className}>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="RAM Compiler" />
+        <meta property="og:description" content="A web-based Random Access Machine compiler and interpreter" />
+        <meta property="og:url" content="https://ram-compile-editor.vercel.app/" />
+        <meta property="og:site_name" content="RAM Compiler" />
+        <meta property="og:image" content="https://ram-compile-editor.vercel.app/opengraph-image.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="twitter:title" content="RAM Compiler" />
+        <meta property="twitter:description" content="A web-based Random Access Machine compiler and interpreter" />
+        <meta property="twitter:image" content="https://ram-compile-editor.vercel.app/twitter-image.png" />
+        <meta name="twitter:card" content="A web-based Random Access Machine compiler and interpreter" />
+        <meta name="twitter:site" content="@Tomas_engineer" />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
